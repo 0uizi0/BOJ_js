e9b2561299ff28bd1d24d8bc0feee787b0f55e44
0uizi0 <0uizi0@naver.com>
2022-09-04 23:13:01 +0900

    #10998 (solved)

--- 1. 입출력과 사칙연산/10998/app.js
+++ 1. 입출력과 사칙연산/10998/app.js
@@ -1,24 +1,8 @@
 let fs = require('fs');
 const filePath = process.platform == 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
 
-let count = input[0];
-let numbers = [];
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
 
-for (let i = 0; i < input.length; i++) {
-  if (input[i] !== '') {
-    numbers.push(input[i].split(' '));
-  }
-}
-
-let num1 = Number(numbers[0][0]);
-let num2 = Number(numbers[0][1]);
-var sum = num1*num2;
-console.log(sum);
-
-if (sum != ''){
-    let num1 = Number(numbers[1][0]);
-    let num2 = Number(numbers[1][1]);
-    var sum = num1*num2;
-    console.log(sum);
-}
+console.log(a*b);
