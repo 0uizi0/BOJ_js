b84c8296c6219a755e4c7d0a331d93ca2c08ccd9
0uizi0 <0uizi0@naver.com>
2022-09-04 23:22:13 +0900

    #1008

---
+++ 1. 입출력과 사칙연산/1008/app.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+const filePath = process.platform == 'linux' ? '/dev/stdin' : 'input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a/b);
