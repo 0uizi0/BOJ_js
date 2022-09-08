82a12a77acaf21dc3b9b526e6f27a6abc81b6d5a
0uizi0 <0uizi0@naver.com>
2022-09-08 23:16:05 +0900

    #14681 (solved)

--- 2. 조건문/14681/app.js
+++ 2. 조건문/14681/app.js
@@ -1,5 +1,7 @@
+// 런타임에러 원인 : dev/stdin
+// 런타임에러 해결방법 : dev/stdin 대신 0 삽입
 let fs = require("fs");
-const filePath = process.platform == "linux" ? "/dev/stdin" : "input.txt";
+const filePath = process.platform == "linux" ? 0 : "input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 let x = parseInt(input[0]);
