54097d68a85774c044b7f5f0c8ce8acd9d5db3be
0uizi0 <0uizi0@naver.com>
2022-09-13 23:54:12 +0900

    #15552

--- 3. 반복문/15552/app.js
+++ 3. 반복문/15552/app.js
@@ -1,18 +1,27 @@
-let fs = require("fs");
-const filePath = process.platform == "linux" ? 0 : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+// 이전 코드의 경우, 매번 console.log로 출력 => 시간초과를 받음.
+// => 하나의 문자열에 결과값과 개행문자를 저장하여 마지막에 출력하도록 재설정
 
-let t = Number(input[0]);
-let numbers = [];
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-for (let i = 1; i < input.length; i++) {
-  if (input[i] !== "") {
-    numbers.push(input[i].split(" "));
+let result = "";
+rl.on("line", (line) => {
+  const input = line.split(" ");
+
+  if (input.length === 2) {
+    const a = Number(input[0]);
+    const b = Number(input[1]);
+    result += a + b + "\n";
   }
-}
+}).on("close", () => {
+  console.log(result);
+  process.exit();
+});
 
-for (let i = 0; i < t; i++) {
-  let a = Number(numbers[i][0]);
-  let b = Number(numbers[i][1]);
-  console.log(a + b);
-}
+// 콘솔에서는 해당 코드 실행 시 입력만 계속 받음
+// => close 이벤트에 걸리지 않았기 때문.
+//     *rl.close()가 없을 경우, 무한으로 입력받음
+// (그러나 백준에서는 이미 데이터 파일이 존재하기 때문에 자동으로 close되어 코드 종료)
