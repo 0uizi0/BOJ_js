02aa0c1f3c8c31173353ec76fcc7d93bb190ece7
0uizi0 <0uizi0@naver.com>
2022-09-17 14:50:47 +0900

    #1110

--- 3. 반복문/1110/app.js
+++ 3. 반복문/1110/app.js
@@ -1,39 +1,26 @@
 let fs = require("fs");
-const { formatWithOptions } = require("util");
 const filePath = process.platform == "linux" ? 0 : "input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split(" ");
+let input = Number(fs.readFileSync(filePath).toString());
 
-let a;
+// 연산할 때 주어진 수를 변경해야하므로 다른 변수에 할당
+let num = input;
 let sum;
-let count;
+let i = 0;
 
 while (true) {
-  if (Number(input[0]) < 10) {
-    a = "0" + input[0];
-  } else {
-    a = input[0];
-  }
+  //주어진 수이ㅡ 십의 자리 수와 일의 자리수에 더하여 새로운 수를 만듦
+  sum = Math.floor(num / 10) + (num % 10);
 
-  let t = parseInt(Number(a) / 10);
-  let o = parseInt(Number(a) % 10);
-  sum = t + o;
+  //주어진 수의 일의 자리수를 십의 자리수로 올리고, 새로운 수의 일의 자리 수를 합한다
+  num = (num % 10) * 10 + (sum % 10);
+  i++;
 
-  if (sum !== a) {
-    count = count + 1;
-
-    if (sum < 10) {
-      a = "0" + sum;
-    } else {
-      a = sum;
-    }
-
-    let t = parseInt(Number(a) / 10);
-    let o = parseInt(Number(a) % 10);
-
-    sum = t + o;
-  } else {
+  if (input == num) {
     break;
   }
+
+  //다음 반복에는 주어진 수 = 새로운 수, 새로운수 = 새로운 수2 가 됨.
+  //이런 식으로 새로운 수 n이 될 것임.
 }
 
-console.log(count);
+console.log(i);
