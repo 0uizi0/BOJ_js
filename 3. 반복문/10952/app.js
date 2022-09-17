937c1ec243dbec31ffc0cbbe172ab7cf6fb77973
0uizi0 <0uizi0@naver.com>
2022-09-17 12:55:38 +0900

    #10952

--- 3. 반복문/10952/app.js
+++ 3. 반복문/10952/app.js
@@ -1,3 +1,25 @@
+// let fs = require("fs");
+// const filePath = process.platform == "linux" ? 0 : "input.txt";
+// let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+// let numbers = [];
+// let result;
+
+// for (let i = 0; i < input.length; i++) {
+//   if (input[i] !== "") {
+//     numbers.push(input[i].split(" "));
+//   }
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//   let a = Number(input[i][0]);
+//   let b = Number(input[i][1]);
+//   result += a + b;
+//   if (a == 0 && b == 0) {
+//     break;
+//   }
+// }
+
 let fs = require("fs");
 const filePath = process.platform == "linux" ? 0 : "input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -12,10 +34,13 @@ for (let i = 0; i < input.length; i++) {
 }
 
 for (let i = 0; i < numbers.length; i++) {
-  let a = Number(input[i][0]);
-  let b = Number(input[i][1]);
-  result += a + b;
-  if (a == 0 && b == 0) {
+  let a = Number(numbers[i][0]);
+  let b = Number(numbers[i][1]);
+  let result = a + b;
+
+  if (result == 0) {
     break;
   }
+
+  console.log(result);
 }
